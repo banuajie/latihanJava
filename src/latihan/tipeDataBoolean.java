da74c98984e9645c9f61dc66ed017b7d6dd43600
banuajie <nugrahabanuajie@gmail.com>
2020-06-07 18:13:25 +0700

    menambahkan file class tipeDataBoolean.java

--- src/latihan/tipeDataBoolean.java
+++ src/latihan/tipeDataBoolean.java
@@ -1,16 +1,21 @@
 package latihan;
 
-public class TestLatihan {
+public class tipeDataBoolean {
     public static void main(String args[]){
         boolean var1, var2, var3, var4, var5;
         
-        var1 = true;
+        var1 = true;    // penulisan true atau false harus kecil semua
         var2 = false;
         
         System.out.println("var1 = " + var1);
         System.out.println("var2 = " + var2);
         
         System.out.println();
+        
+        /*
+            Umumnya, tipe data boolean di dapat dari hasil operasi perbandingan,
+            seperti apakah sebuah angka sama dengan angka lain, apakah lebih besar atau lebih kecil, dst.
+        */
         
         var3 = 12 < 10;
         var4 = 30 > 25;
